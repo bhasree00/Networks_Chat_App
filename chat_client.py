d863f6bb4cc3cd94bd53c0eadc9d152fbf4f6b37
Bharat <bbskkd@umsystem.edu>
2021-11-27 15:12:08 -0600

    Still trying

--- chat_client.py
+++ chat_client.py
@@ -49,33 +49,6 @@ def main():
         elif screenMode == 3: # Quit mode
             return
 
-
-
-    # while True:
-    #     for event in pg.event.get():
-    #         if event.type == pg.QUIT:
-    #             return
-    #         elif event.type == pg.KEYDOWN:
-    #             if event.key == pg.K_RETURN:
-    #                 print(text)
-    #                 text = ''
-    #                 # Wait for user to input a message
-    #                 # message = input(f'{MY_USERNAME} > ')
-    #                 message = text
-    #                 # If message is not empty - send it
-    #                 if message:
-    #                     # Encode message to bytes, prepare header and convert to bytes, like for username above, then send
-    #                     message = message.encode('utf-8')
-    #                     message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
-    #                     client_socket.send(message_header + message)
-    #             elif event.key == pg.K_BACKSPACE:
-    #                 text = text[:-1]
-    #             else:
-    #                 text += event.unicode
-
-        
-        
-
 def sendMessage(screen, font, clock, color):
     text = ""
     while True:
@@ -95,67 +68,78 @@ def sendMessage(screen, font, clock, color):
                         message = message.encode('utf-8')
                         message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
                         client_socket.send(message_header + message)
-                        return 
+                        return 2
                 elif event.key == pg.K_BACKSPACE:
                     text = text[:-1]
                 else:
                     text += event.unicode
         
         screen.fill(BLACK)
-        txt_surface = font.render(text, True, color)
+        thing_to_print = MY_USERNAME + " > " + text
+        txt_surface = font.render(thing_to_print, True, color)
         screen.blit(txt_surface, (50, 100))
 
         pg.display.flip()
         clock.tick(30)
 
 def displayMessage(screen, font, clock, color):
-    try:
-        # Now we want to loop over received messages (there might be more than one) and print them
-        # while True:
+    client_socket.send("\n".encode('utf-8'))
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return 3
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_RETURN:
+                    return 1
 
-        # Receive our "header" containing username length, it's size is defined and constant
-        username_header = client_socket.recv(HEADER_LENGTH)
+        try:
+            # Now we want to loop over received messages (there might be more than one) and print them
+            # while True:
 
-        # If we received no data, server gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
-        if not len(username_header):
-            print('Connection closed by the server')
+            # Receive our "header" containing username length, it's size is defined and constant
+            username_header = client_socket.recv(HEADER_LENGTH)
+
+            # If we received no data, server gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
+            if not len(username_header):
+                print('Connection closed by the server')
+                sys.exit()
+
+            # Convert header to int value
+            username_length = int(username_header.decode('utf-8').strip())
+
+            # Receive and decode username
+            username = client_socket.recv(username_length).decode('utf-8')
+
+            # Now do the same for message (as we received username, we received whole message, there's no need to check if it has any length)
+            message_header = client_socket.recv(HEADER_LENGTH)
+            message_length = int(message_header.decode('utf-8').strip())
+            responseText = client_socket.recv(message_length).decode('utf-8')
+
+            # Print message
+            print(f'{username} > {responseText}')
+            # return 1
+        except IOError as e:
+            # This is normal on non blocking connections - when there are no incoming data error is going to be raised
+            # Some operating systems will indicate that using AGAIN, and some using WOULDBLOCK error code
+            # We are going to check for both - if one of them - that's expected, means no incoming data, continue as normal
+            # If we got different error code - something happened
+            if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+                print('Reading error: {}'.format(str(e)))
+                sys.exit()
+
+            # We just did not receive anything
+            return 3
+        except Exception as e:
+            # Any other exception - something happened, exit
+            print('Reading error: '.format(str(e)))
             sys.exit()
+        
+        screen.fill(BLACK)
+        txt_surface = font.render(responseText, True, color)
+        screen.blit(txt_surface, (50, 100))
 
-        # Convert header to int value
-        username_length = int(username_header.decode('utf-8').strip())
-
-        # Receive and decode username
-        username = client_socket.recv(username_length).decode('utf-8')
-
-        # Now do the same for message (as we received username, we received whole message, there's no need to check if it has any length)
-        message_header = client_socket.recv(HEADER_LENGTH)
-        message_length = int(message_header.decode('utf-8').strip())
-        responseText = client_socket.recv(message_length).decode('utf-8')
-
-        # Print message
-        print(f'{username} > {responseText}')
-    except IOError as e:
-        # This is normal on non blocking connections - when there are no incoming data error is going to be raised
-        # Some operating systems will indicate that using AGAIN, and some using WOULDBLOCK error code
-        # We are going to check for both - if one of them - that's expected, means no incoming data, continue as normal
-        # If we got different error code - something happened
-        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-            print('Reading error: {}'.format(str(e)))
-            sys.exit()
-
-        # We just did not receive anything
-        return 3
-    except Exception as e:
-        # Any other exception - something happened, exit
-        print('Reading error: '.format(str(e)))
-        sys.exit()
-    
-    screen.fill(BLACK)
-    txt_surface = font.render(responseText, True, color)
-    screen.blit(txt_surface, (50, 100))
-
-    pg.display.flip()
-    clock.tick(30)
+        pg.display.flip()
+        clock.tick(30)
 
 
 
